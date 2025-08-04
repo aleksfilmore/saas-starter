7620456ee47fa251fb4112e68a9298fb55ee6f65
Aleks F <iamalexfilip@gmail.com>
2025-08-04 09:05:37 +0200

    5421353253

--- app/(marketing)/privacy/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPage() {
-  return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
-      <p>Privacy policy content coming soon...</p>
-    </div>
-  )
-}
