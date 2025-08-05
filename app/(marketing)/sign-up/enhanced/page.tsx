6686482fc3ab820ed9751500596793f17299326e
Aleks F <iamalexfilip@gmail.com>
2025-08-05 10:36:45 +0200

    623632623632636

---
+++ app/(marketing)/sign-up/enhanced/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+
+export default function EnhancedSignUpPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Enhanced Sign Up</h1>
+      <p>Enhanced registration flow coming soon...</p>
+    </div>
+  )
+}
